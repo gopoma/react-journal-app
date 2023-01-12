11bc60f9f5b3b9fc84c41105e55043f681a6653a
gopoma <gordono@unsa.edu.pe>
2023-01-12 02:03:37 -0500

    Added register and login pages

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,14 @@
+import {JournalLayout} from "../layout";
+import {Typography} from "@mui/material";
+import {MailOutline} from "@mui/icons-material";
+
+export const JournalPage = () => {
+    return (
+        <JournalLayout>
+            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel soluta dicta, harum minus autem pariatur laboriosam facilis? Quae, minus rerum dolores eaque eos debitis, quam officia facere cum sequi accusantium!</Typography>
+
+            {/* NothingSelected */}
+            {/* NoteView */}
+        </JournalLayout>
+    );
+};
